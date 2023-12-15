04d718a95bd36c3926a313d2e3d6174df63c31d8
AbsoluteZer0527 <zhuluyuan200527@gmail.com>
2023-12-14 18:07:26 -0800

    initial commit

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,4 @@
-console.log('Hellow everybody');
+document.getElementById('window').addEventListener('click', function() {
+    // Redirect to the "quest" HTML file when clicked
+    window.location.href = 'quest.html';
+});
